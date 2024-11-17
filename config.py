8fd914172a25a7167f8dacbd447dd5be1addf443
Colin <164561623+cesmit27@users.noreply.github.com>
2024-11-17 12:58:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 class Config:
-    SECRET_KEY = 'balls'
+    SECRET_KEY = 'its a secret lol'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///leaf_blower.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
